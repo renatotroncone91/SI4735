ff79829c91750d88840c9cc517bb78df811de4e7
Renato <renato.troncone91@gmail.com>
2026-01-04 22:25:44 +0100

    Add signal bars and rename bands

--- examples/SI47XX_06_ESP32/ESP32_TWO_ENCODERS_NO_MENU/ESP32_TWO_ENCODERS_NO_MENU.ino
+++ examples/SI47XX_06_ESP32/ESP32_TWO_ENCODERS_NO_MENU/ESP32_TWO_ENCODERS_NO_MENU.ino
@@ -79,22 +79,22 @@ struct Band {
 
 Band amBands[] = {
   {"LW", LW_BAND_TYPE, 150, 283, 198, 0, 198},
-  {"MW1", MW_BAND_TYPE, 520, 1710, 1000, 3, 1000},
-  {"MW2", MW_BAND_TYPE, 531, 1701, 783, 2, 783},
-  {"MW3", MW_BAND_TYPE, 1700, 3500, 2500, 1, 2500},
+  {"MW", MW_BAND_TYPE, 520, 1710, 1000, 3, 1000},
+  {"MW-EU", MW_BAND_TYPE, 531, 1701, 783, 2, 783},
+  {"160M", MW_BAND_TYPE, 1700, 3500, 2500, 1, 2500},
   {"80M", SW_BAND_TYPE, 3500, 4000, 3700, 1, 3700},
-  {"SW1", SW_BAND_TYPE, 4000, 5500, 4885, 1, 4885},
-  {"SW2", SW_BAND_TYPE, 5500, 6500, 6000, 1, 6000},
+  {"SW4-5", SW_BAND_TYPE, 4000, 5500, 4885, 1, 4885},
+  {"SW5-6", SW_BAND_TYPE, 5500, 6500, 6000, 1, 6000},
   {"40M", SW_BAND_TYPE, 6500, 7300, 7100, 1, 7100},
-  {"SW3", SW_BAND_TYPE, 7200, 8000, 7200, 1, 7200},
-  {"SW4", SW_BAND_TYPE, 9000, 11000, 9500, 1, 9500},
-  {"SW5", SW_BAND_TYPE, 11100, 13000, 11900, 1, 11900},
-  {"SW6", SW_BAND_TYPE, 13000, 14000, 13500, 1, 13500},
+  {"SW7-8", SW_BAND_TYPE, 7200, 8000, 7200, 1, 7200},
+  {"SW9-11", SW_BAND_TYPE, 9000, 11000, 9500, 1, 9500},
+  {"SW11-13", SW_BAND_TYPE, 11100, 13000, 11900, 1, 11900},
+  {"SW13-14", SW_BAND_TYPE, 13000, 14000, 13500, 1, 13500},
   {"20M", SW_BAND_TYPE, 14000, 15000, 14200, 1, 14200},
-  {"SW7", SW_BAND_TYPE, 15000, 17000, 15300, 1, 15300},
-  {"SW8", SW_BAND_TYPE, 17000, 18000, 17500, 1, 17500},
+  {"SW15-17", SW_BAND_TYPE, 15000, 17000, 15300, 1, 15300},
+  {"SW17-18", SW_BAND_TYPE, 17000, 18000, 17500, 1, 17500},
   {"15M", SW_BAND_TYPE, 20000, 21400, 21100, 1, 21100},
-  {"SW9", SW_BAND_TYPE, 21400, 22800, 21500, 1, 21500},
+  {"SW21-22", SW_BAND_TYPE, 21400, 22800, 21500, 1, 21500},
   {"CB", SW_BAND_TYPE, 26000, 28000, 27500, 1, 27500},
   {"10M", SW_BAND_TYPE, 28000, 30000, 28400, 1, 28400},
   {"ALL", SW_BAND_TYPE, 150, 30000, 15000, 3, 15000}
@@ -186,6 +186,9 @@ uint16_t currentStep() {
 }
 
 void showStatus() {
+  uint8_t barSegments = min<uint8_t>(currentRssi / 6, 15);
+  uint8_t barWidth = barSegments * 4;
+
   display.clearDisplay();
   display.setTextColor(SSD1306_WHITE);
   display.setCursor(0, 0);
@@ -226,6 +229,10 @@ void showStatus() {
   display.print(currentSnr);
   if (currentMode == MODE_FM) {
     display.print(currentStereo ? " ST" : " MO");
+  }
+  display.drawRect(0, 58, 64, 6, SSD1306_WHITE);
+  if (barWidth > 0) {
+    display.fillRect(1, 59, min<uint8_t>(barWidth, 62), 4, SSD1306_WHITE);
   }
   display.display();
 }
